ad6fd68b1223a1e2c736fef4d0a72eaddbce78c5
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-07-18 13:26:22 +0700

    update

---
+++ src/array-methods.js
@@ -0,0 +1,20 @@
+const faker = require('faker')
+const _ = require('lodash')
+const fs = require('fs')
+
+const roles = ['Manager', 'Technical', 'SA', 'HR']
+
+const employees = Array(100)
+  .fill(null)
+  .map(() => ({
+    name: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    age: _.random(20, 50),
+    salary: _.random(20000, 50000),
+    role: roles[_.random(0, roles.length - 1)],
+  }))
+
+fs.writeFileSync(`${__dirname}/employees.json`, JSON.stringify(employees))
+
+// console.log(employees)
