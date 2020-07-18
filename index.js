b3f0a72df448d0c18882d1e70dfce96a66f8153a
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-07-18 10:07:49 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1>hello world update feature!!</h1>')
+  } else if (req.url === '/api') {
+    const data = { name: 'myname', lastName: 'mylastname' }
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(data))
+  }
+})
+
+server.listen(3000, () => {
+  console.log('Server listen on port 3000')
+})
