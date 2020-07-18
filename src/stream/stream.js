4bd90e18610992a555267fd17cded516b98c3716
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-07-18 16:32:42 +0700

    update

--- src/stream/stream.js
+++ src/stream/stream.js
@@ -1,1 +1,19 @@
 const fs = require('fs')
+const path = require('path')
+
+const url = path.join(__dirname, '..', 'employees.json')
+
+const data = fs.createReadStream(url, {
+  encoding: 'utf-8',
+  highWaterMark: 10,
+})
+
+const copyUrl = path.join(__dirname, 'mycopyfile.json')
+
+const file = fs.createWriteStream(copyUrl)
+
+data.on('data', function (chunk) {
+  // file.write(process.stdout)
+  process.stdout.write(chunk)
+  file.write(chunk.toUpperCase())
+})
