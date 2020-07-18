c88d867bc9c18d36d6f032e263bb20be72118e50
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-07-18 10:13:49 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>hello world update feature!!</h1>')
+    res.end('<h1>hello world update test feature!!</h1>')
   } else if (req.url === '/api') {
     const data = { name: 'myname', lastName: 'mylastname' }
     res.setHeader('Content-Type', 'application/json')
