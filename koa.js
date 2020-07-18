2763f7f7e9d8e16b0dbbded82c4bc63030eac740
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-07-18 10:31:37 +0700

    update

--- koa.js
+++ koa.js
@@ -1,11 +1,21 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 
 const app = new Koa()
+const r = new Router()
 
-app.use(ctx => {
-  ctx.body = 'hello world'
+r.get('/', ctx => {
+  ctx.body = '<h1>hello world</h1>'
 })
 
+r.get('/api', ctx => {
+  ctx.body = {
+    name: 'myname',
+    lastName: 'mylastname',
+  }
+})
+
+app.use(r.routes())
 const port = 3000
 app.listen(port, () => {
   console.log(`Server listen on port ${port}`)
